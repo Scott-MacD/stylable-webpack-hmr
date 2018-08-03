429008de8b4bd73f4a41a663885f0538296ef01f
Scott MacDonald <scott@subscript.ca>
2018-08-03 16:15:57 -0400

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const StylableWebpackPlugin = require('stylable-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: "development",
+    plugins: [
+        new StylableWebpackPlugin({
+            rootScope: false
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        inline: true,
+        port: 3001,
+        hot: true
+    }
+}
